3e59ce756d4df362bdf1c6e659a57dad0b9b4682
Bilek-Petr <p.bilek29@gmail.com>
2024-08-05 14:51:46 +0200

    added feature that only 1 item of same value can be in cart

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from 'react';
 
 export default function ProductList() {
    const [data, setData] = useState([]);
+   const [cart, setCart] = useState([]);
 
    useEffect(() => {
       const fetchData = async () => {
          try {
-            const response = await fetch('../public/assets/data/data.json');
+            const response = await fetch('/assets/data/data.json');
             const result = await response.json();
             setData(result);
          } catch (err) {
@@ -18,10 +19,32 @@ export default function ProductList() {
       fetchData();
    }, []);
 
+   const addToCart = (product) => {
+      setCart((prevCart) => {
+         const existingProduct = prevCart.find((item) => item.name === product.name);
+         if (existingProduct) {
+            return prevCart.map((item) =>
+               item.name === product.name
+                  ? { ...item, quantity: item.quantity + product.quantity }
+                  : item
+            );
+         }
+         return [...prevCart, product];
+      });
+   };
+
+   console.log(cart);
+
    return (
       <ul className="product-list">
          {data.map((product) => {
-            return <ProductItem key={crypto.randomUUID()} product={product} />;
+            return (
+               <ProductItem
+                  key={crypto.randomUUID()}
+                  product={product}
+                  addToCart={addToCart}
+               />
+            );
          })}
       </ul>
    );
