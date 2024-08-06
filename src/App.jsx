452d2d0bd653c44812e4d7f319c56c4661eeca2b
Bilek-Petr <p.bilek29@gmail.com>
2024-08-06 13:11:52 +0200

    connected style files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 import './App.css';
+import './assets/styles/main.scss';
 
 function App() {
    return (
