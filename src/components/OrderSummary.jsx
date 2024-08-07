a85e9d42d9a9ab993f12e27dea56d27a98514066
Bilek-Petr <p.bilek29@gmail.com>
2024-08-07 10:30:26 +0200

    added classNames

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -7,40 +7,54 @@ export default function OrderSummary({ cartItems, onStartNewOrder }) {
 
    return (
       <div className="order-tab">
-         <div>
+         <div className="order-tab__img">
             <img
                src="/assets/images/icon-order-confirmed.svg"
                alt="order confirmed icon"
             />
          </div>
-         <p>Order Confirmed</p>
-         <p>We hope you enjoy your food!</p>
-         <ul>
+         <p className="order-tab__title">Order Confirmed</p>
+         <p className="order-tab__desc">We hope you enjoy your food!</p>
+         <ul className="order-tab__list">
             {cartItems.map((item) => {
                return (
                   <>
-                     <li key={item.name}>
-                        <div>
+                     <li className="order-tab__list-item" key={item.name}>
+                        <div className="order-tab__list-item-img">
                            <img src={item.image.thumbnail} alt={item.name} />
                         </div>
-                        <div>
-                           <p>{item.name}</p>
-                           <p>{item.quantity}x</p>
-                           <p>${item.price}</p>
+                        <div className="order-tab__list-item-info">
+                           <p className="order-tab__item-info-name | cart__list-item-name">
+                              {item.name}
+                           </p>
+                           <div className="flex gap-1">
+                              <p className="order-tab__item-info-qty | cart__list-item-qty">
+                                 {item.quantity}x
+                              </p>
+                              <p className="order-tab__item-info-price">
+                                 @ ${item.price.toFixed(2)}
+                              </p>
+                           </div>
                         </div>
-                        <div>
-                           <p>{(item.quantity * item.price).toFixed(2)}</p>
+                        <div className="order-tab-wrapper">
+                           <p className="order-tab__total">
+                              ${(item.quantity * item.price).toFixed(2)}
+                           </p>
                         </div>
                      </li>
                   </>
                );
             })}
-            <div>
-               <p>Order Total</p>
-               <p>${totalPrice}</p>
+            <div className="order-tab__sum | cart__total">
+               <p className="tab-sum__text">Order Total</p>
+               <p className="tab-sum__price | cart__total-price">
+                  ${totalPrice.toFixed(2)}
+               </p>
             </div>
 
-            <button onClick={onStartNewOrder}>Start New Order</button>
+            <button className="btn btn--new-order" onClick={onStartNewOrder}>
+               Start New Order
+            </button>
          </ul>
       </div>
    );
